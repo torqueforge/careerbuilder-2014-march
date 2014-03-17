4229fe3af170d24a53744584e744ae6e55d36cf7
Katrina Owen <katrina.owen@gmail.com>
2014-03-17 10:11:59 -0400

    Skip all but the first unicorn test

--- unicorn/test/unicorn_test.rb
+++ unicorn/test/unicorn_test.rb
@@ -10,36 +10,43 @@ class UnicornTest < Minitest::Test
   end
 
   def test_it_can_be_named_something_else
+    skip
     unicorn = Unicorn.new("Joseph")
     assert_equal "Joseph", unicorn.name
   end
 
   def test_it_is_white_by_default
+    skip
     unicorn = Unicorn.new("Margaret")
     assert_equal "white", unicorn.color
   end
 
   def test_it_knows_if_it_is_white
+    skip
     unicorn = Unicorn.new("Elisabeth")
     assert unicorn.white?, "Elisabeth should be white, but isn't."
   end
 
   def test_it_does_not_have_to_be_white
+    skip
     unicorn = Unicorn.new("Barbara", "purple")
     assert_equal "purple", unicorn.color
   end
 
   def test_it_knows_if_it_is_not_white
+    skip
     unicorn = Unicorn.new("Roxanne", "green")
     refute unicorn.white?, "I guess Roxanne thinks she's white, when really she is green."
   end
 
   def test_unicorn_says_sparkly_stuff
+    skip
     unicorn = Unicorn.new("Johnny")
     assert_equal "**;* Wonderful! **;*", unicorn.say("Wonderful!")
   end
 
   def test_unicorn_says_different_sparkly_stuff
+    skip
     unicorn = Unicorn.new("Francis")
     assert_equal "**;* I don't like you very much. **;*", unicorn.say("I don't like you very much.")
   end
