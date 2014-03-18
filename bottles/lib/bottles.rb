b64647e5daa0c2fd3b612726079f32d4f6dc38bf
Katrina Owen <katrina.owen@gmail.com>
2014-03-18 14:51:42 -0400

    Add DRY bottles solution

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,6 +1,8 @@
 class Bottles
+  MAX_VERSES = 99
+
   def sing
-    verses(99, 0)
+    verses(MAX_VERSES, 0)
   end
 
   def verses(upper_bound, lower_bound)
@@ -10,15 +12,67 @@ class Bottles
   end
 
   def verse(verse_number)
+    "#{current_how_many(verse_number).capitalize} #{current_container(verse_number)} of beer on the wall, "+
+    "#{current_how_many(verse_number)} #{current_container(verse_number)} of beer.\n"+
+    "#{action(verse_number)}, "+
+    "#{remaining_how_many(verse_number)} #{remaining_container(verse_number)} of beer on the wall.\n"
+  end
+
+  private
+
+  def action(verse_number)
     case verse_number
     when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      'Go to the store and buy some more'
     else
-      "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\nTake one down and pass it around, #{verse_number-1} bottles of beer on the wall.\n"
+      "Take #{pronoun(verse_number)} down and pass it around"
+    end
+  end
+
+  def current_container(verse_number)
+    case verse_number
+    when 1
+      'bottle'
+    else
+      'bottles'
+    end
+  end
+
+  def remaining_container(verse_number)
+    case verse_number
+    when 2
+      'bottle'
+    else
+      'bottles'
+    end
+  end
+
+  def current_how_many(verse_number)
+    case verse_number
+    when 0
+      'no more'
+    else
+      verse_number.to_s
+    end
+  end
+
+  def remaining_how_many(verse_number)
+    case verse_number
+    when 0
+      MAX_VERSES
+    when 1
+      'no more'
+    else
+      verse_number - 1
+    end
+  end
+
+  def pronoun(verse_number)
+    case verse_number
+    when 1
+      'it'
+    else
+      'one'
     end
   end
 end
