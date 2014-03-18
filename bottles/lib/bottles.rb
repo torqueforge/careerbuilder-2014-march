798702b593d793694f4381db621be3f8551127d5
Katrina Owen <katrina.owen@gmail.com>
2014-03-18 10:47:59 -0400

    Tidy up more lowest hanging green

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -11,12 +11,12 @@ class Bottles
 
   def verse(verse_number)
     case verse_number
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     when 0
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    when 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     else
       "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\nTake one down and pass it around, #{verse_number-1} bottles of beer on the wall.\n"
     end
