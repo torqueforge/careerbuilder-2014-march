037fb6f940690f86da9ea9718ba8131f38669f29
Katrina Owen <katrina.owen@gmail.com>
2014-03-18 10:45:44 -0400

    Tidy up the lowest hanging green

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,24 +1,24 @@
 class Bottles
+  def sing
+    verses(99, 0)
+  end
 
-  def verse arg1
-    case arg1
+  def verses(upper_bound, lower_bound)
+    upper_bound.downto(lower_bound).map {|i|
+      verse(i) << "\n"
+    }.join
+  end
+
+  def verse(verse_number)
+    case verse_number
     when 2
-       "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     when 0
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
-       "#{arg1} bottles of beer on the wall, #{arg1} bottles of beer.\nTake one down and pass it around, #{arg1-1} bottles of beer on the wall.\n"
+      "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\nTake one down and pass it around, #{verse_number-1} bottles of beer on the wall.\n"
     end
-  end
-
-  def verses(arg1, arg2)
-    arg1.downto(arg2).map {|number|
-    verse(number) << "\n"}.join
-  end
-
-  def sing
-   verses(99,0)
   end
 end
