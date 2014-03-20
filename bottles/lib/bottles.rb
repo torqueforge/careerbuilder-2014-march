d9f71fb62edcf357ec1135b75c441cc8faf365d9
Sandi Metz <skmetz@gmail.com>
2014-03-20 07:19:58 -0400

    deleted unuse VerseStrategies

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -19,18 +19,12 @@ end
 
 
 
-require 'forwardable'
-
 class Verse
 
-  extend Forwardable
-  delegate [:current_container, :current_how_many, :action, :remaining_how_many, :remaining_container] => :strategy
-
-  attr_reader :strategy, :number
+  attr_reader :number
 
   def initialize(number)
     @number   = number
-    @strategy = SuitableVerseStrategy.for(number)
   end
 
   def to_s
@@ -117,91 +111,3 @@ class BeerSongBottleNumber1 < BeerSongBottleNumber
 end
 
 
-
-
-class VerseStrategy
-  MAX_VERSES = 99
-
-  attr_reader :number
-
-  def initialize(number)
-    @number = number
-  end
-
-  def action
-    "Take #{pronoun} down and pass it around"
-  end
-
-  def current_container
-    'bottles'
-  end
-
-  def remaining_container
-    'bottles'
-  end
-
-  def current_how_many
-    number.to_s
-  end
-
-  def remaining_how_many
-    number - 1
-  end
-
-  def pronoun
-    'one'
-  end
-
-end
-
-class VerseStrategy0 < VerseStrategy
-
-  def action
-    'Go to the store and buy some more'
-  end
-
-  def current_how_many
-    'no more'
-  end
-
-  def remaining_how_many
-    MAX_VERSES
-  end
-
-end
-
-class VerseStrategy1 < VerseStrategy
-
-  def current_container
-    'bottle'
-  end
-
-  def remaining_how_many
-    'no more'
-  end
-
-  def pronoun
-    'it'
-  end
-
-end
-
-class VerseStrategy2 < VerseStrategy
-
-  def remaining_container
-    'bottle'
-  end
-end
-
-
-
-class SuitableVerseStrategy
-
-  def self.for(number)
-    if Object.const_defined?("VerseStrategy#{number}")
-      Object.const_get("VerseStrategy#{number}")
-    else
-      VerseStrategy
-    end.new(number)
-  end
-end
