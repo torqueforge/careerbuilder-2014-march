02fff77d0a9e6403dff0be49a7258674fa35bdb1
Sandi Metz <skmetz@gmail.com>
2014-03-20 07:18:18 -0400

    added BeerSongBottleNumber, still have VerseStrategy

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -26,20 +26,96 @@ class Verse
   extend Forwardable
   delegate [:current_container, :current_how_many, :action, :remaining_how_many, :remaining_container] => :strategy
 
-  attr_reader :strategy
+  attr_reader :strategy, :number
 
   def initialize(number)
+    @number   = number
     @strategy = SuitableVerseStrategy.for(number)
   end
 
   def to_s
-    "#{current_how_many.capitalize} #{current_container} of beer on the wall, "+
-    "#{current_how_many} #{current_container} of beer.\n"+
-    "#{action}, "+
-    "#{remaining_how_many} #{remaining_container} of beer on the wall.\n"
+    bottle_number =
+      case number
+      when 0
+        BeerSongBottleNumber0.new(number)
+      when 1
+        BeerSongBottleNumber1.new(number)
+      else
+        BeerSongBottleNumber.new(number)
+      end
+
+    "#{bottle_number.how_many.capitalize} #{bottle_number.container} of beer on the wall, "+
+    "#{bottle_number.how_many} #{bottle_number.container} of beer.\n"+
+    "#{bottle_number.action}, "+
+    "#{bottle_number.next.how_many} #{bottle_number.next.container} of beer on the wall.\n"
   end
 
 end
+
+
+class BeerSongBottleNumber
+
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def action
+    "Take #{pronoun} down and pass it around"
+  end
+
+  def container
+    'bottles'
+  end
+
+  def how_many
+    number.to_s
+  end
+
+  def pronoun
+    'one'
+  end
+
+  def next
+    pred = number.pred
+
+    case pred
+    when -1
+      BeerSongBottleNumber.new(99)
+    when 0
+      BeerSongBottleNumber0.new(pred)
+    when 1
+      BeerSongBottleNumber1.new(pred)
+    else
+      BeerSongBottleNumber.new(pred)
+    end
+  end
+
+end
+
+class BeerSongBottleNumber0 < BeerSongBottleNumber
+
+  def action
+    'Go to the store and buy some more'
+  end
+
+  def how_many
+    'no more'
+  end
+end
+
+class BeerSongBottleNumber1 < BeerSongBottleNumber
+  def container
+    'bottle'
+  end
+
+  def pronoun
+    'it'
+  end
+
+end
+
 
 
 
